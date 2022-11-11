af3532bcc24b31800e08be1ebd0601ea7814eed0
Khang <kt36@njit.edu>
2022-11-11 12:29:00 -0500

    init

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import os
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
+os.environ["CUDA_VISIBLE_DEVICES"]="4"
+os.environ["CUDA_LAUNCH_BLOCKING"]="1"
 from Utils.utils import *
 from config import parse_args
 from Datasets.PET import *
